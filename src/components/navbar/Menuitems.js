6ba66137d51347616d4f6201188c22c47004fbd3
Adejare <inioluwaadesida@gmail.com>
2022-04-17 18:31:29 +0100

    first commit

---
+++ src/components/navbar/Menuitems.js
@@ -0,0 +1,22 @@
+export const Menuitems = [
+    {
+        title: "HOME",
+        url: "/",
+        cnames: "nav-links"
+    },
+    {
+        title: "ABOUT",
+        url: "/",
+        cnames: "nav-links"
+    },
+    {
+        title: "PROJECTS",
+        url: "/",
+        cnames: "nav-links"
+    },
+    {
+        title: "CONTACT",
+        url: "/",
+        cnames: "nav-links"
+    }
+]
