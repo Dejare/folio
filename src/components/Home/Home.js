41f0254c77530b184b53d6ac1cd58ad9f04a16f7
Adejare <inioluwaadesida@gmail.com>
2022-04-17 20:07:31 +0100

    footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,17 @@ import Navbar from '../navbar/Navbar'
 import './home.scss'
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
         <Navbar/>
         <div className='hero'><h1>
             DEJJ
             </h1></div>
+            <div>
+                <footer>
+                    &copy; 2022 <br/>
+                    -WAGMI
+                </footer>
+            </div>
     </div>
   )
 }
