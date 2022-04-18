2221f65635a491d8e54eb377229689d1f1bf6dfa
Adejare <inioluwaadesida@gmail.com>
2022-04-18 15:50:34 +0100

    about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from './components/Home/Home';
-import './main.scss'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/about/About";
+import Home from "./components/Home/Home";
+import "./main.scss";
 function App() {
-  return (
-    <Router>
-      <Home/>
-  
-   
-    </Router>
-  )
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+
+                <Route path="/about" exact>
+                    <About/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
