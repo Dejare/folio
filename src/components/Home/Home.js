7491652ad9c985428ad6d3d6ce951651ba18b397
Adejare <inioluwaadesida@gmail.com>
2022-04-18 08:32:27 +0100

    hero

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,16 @@ const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
-        <div className='hero'><h1>
-            //Help
-            </h1></div>
+        <div className='hero'>
+          <div className='text'>
+            <p>Adejare Adesida</p>
+            <h1>Self-taught <span>Frontend Developer,</span></h1>
+            <div>
+            <h1>School-taught <span>Computer Engineer.</span></h1>
+            <p>I build websites with a focus on responsive design and accessibility.</p>
+            </div>
+           </div>
+        </div>
             <div>
                 <footer>
                     &copy; 2022 <br/>
