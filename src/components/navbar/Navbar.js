30a8840a75b7c954806472d9a689abde91fa06e8
Adejare <inioluwaadesida@gmail.com>
2022-04-18 06:58:46 +0100

    nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,40 +1,50 @@
-import React, {useState} from 'react'
-import {Menuitems} from './Menuitems'
-import './navbar.scss'
-import {BiMenuAltRight} from "react-icons/bi"
-import {FaTimes} from "react-icons/fa"
-import {AiFillGithub, AiFillTwitterSquare, AiFillLinkedin} from "react-icons/ai"
+import React, { useState } from "react";
+import { Menuitems } from "./Menuitems";
+import "./navbar.scss";
+import { BiMenuAltRight } from "react-icons/bi";
+import { FaTimes } from "react-icons/fa";
+import {
+    AiFillGithub,
+    AiFillTwitterSquare,
+    AiFillLinkedin,
+} from "react-icons/ai";
 const Navbar = () => {
-    const [click, setclick] = useState(false)
+    const [click, setclick] = useState(false);
     function handleClick() {
-        setclick(!click)
+        setclick(!click);
     }
-  return (
-    <>
-    <header>
-        <div className='navLogo'>DEJJ</div>
-        <nav className={click ? "navactive" : "nav"} >
-            <div className='menuIcon' onClick={handleClick}>
-                {click ?  <FaTimes className='menuIcon'/> : <BiMenuAltRight className='menuIcon'/>}
-            </div>
-            <ul className={click ? "navmenu active" : "navmenu"}>
-                {
-                    Menuitems.map((item, index)=> {
-                        return(
-                            <li key={index}><a className={item.cname} href={item.url}>{item.title}</a></li>
-                        )
-                    })
-}               
-            </ul>
-            <div className='socialIcons'>
-                <AiFillGithub  className='icon'/>
-                <AiFillLinkedin className='icon'/>
-                <AiFillTwitterSquare className='icon'/>
-            </div>
-        </nav>
-    </header>
-    </>
-  )
-}
+    return (
+        <>
+            <header>
+                <div className="navLogo">DEJJ</div>
+                <nav className={click ? "navactive" : "nav"}>
+                    <ul className={click ? "navmenu active" : "navmenu"}>
+                        {Menuitems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a className={item.cname} href={item.url}>
+                                        {item.title}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <div className="menuIcon" onClick={handleClick}>
+                        {click ? (
+                            <FaTimes className="close" />
+                        ) : (
+                            <BiMenuAltRight className="menuIcon" />
+                        )}
+                    </div>
+                    <div className="socialIcons">
+                        <AiFillGithub className="icon" />
+                        <AiFillLinkedin className="icon" />
+                        <AiFillTwitterSquare className="icon" />
+                    </div>
+                </nav>
+            </header>
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;
