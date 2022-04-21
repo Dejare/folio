294acefe63ca88c050864411302c8d7239b8f6ce
Adejare <inioluwaadesida@gmail.com>
2022-04-21 21:30:03 +0100

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
             </div>
             <div className='paragraph'>
                 &copy; 2022 <br/>
-                Dejare- Development.
+                Dejare
             </div>
         </div>
     </div>
